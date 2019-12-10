8d74ac515cc6779a82040af7394b28d11eb3bdf2
Linda Saraguro <lindasaraguro@yahoo.com>
2019-12-10 14:43:13 -0500

    refactor: colors component

--- app/routes/colors.js
+++ app/routes/colors.js
@@ -1,8 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default class ColorsRoute extends Route {
-    // model(){
-    //     return ["Grace Hopper","Linda Saraguro", "Hariet Lewis"]
-    // }
+    async model(){
+        // model hook/method fetches any data for the route
+        let response = await fetch(`/api/colors.json`);
+        // the Browser's fetch API requests the JSON data from our server's API at "public/api/colors.json"
+        let { data } = await response.json();
+        // parsing JSON data 
+
+        return data.map(model => {
+            let { id, attributes } = model;
+            // extracting the nested attributes object
+              return { id, ...attributes };
+        })
+    }
 }
 
