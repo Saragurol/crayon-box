bda689a6cdaf3a97deb74c9898e2416afd2cd2fa
Linda Saraguro <lindasaraguro@yahoo.com>
2019-12-10 13:31:02 -0500

    refactor: colors default page

---
+++ app/routes/colors.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default class ColorsRoute extends Route {
+    // model(){
+    //     return ["Grace Hopper","Linda Saraguro", "Hariet Lewis"]
+    // }
+}
+
