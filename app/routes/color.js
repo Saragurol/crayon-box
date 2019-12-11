6dc6d255237cd748d47fc58e2d3acf34f3a69b5b
Linda Saraguro <lindasaraguro@yahoo.com>
2019-12-11 01:59:08 -0500

    refactor: changed the index template, feat:added colordetails component

--- app/routes/color.js
+++ app/routes/color.js
@@ -1,13 +1,11 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
-export default class ColorsRoute extends Route {
-    async model(params){
-        // model hook/method fetches any data for the route
-        let response = await fetch(`/api/colors/${params.color_id}.json`);
-        // the Browser's fetch API requests the JSON data from our server's API at "public/api/colors.json"
-        let { data } = await response.json();
-        // parsing JSON data 
-        let { id, attributes } = data;
-        return { id, attributes }
-    }
+export default class ColorRoute extends Route {
+  async model(params) {
+    let response = await fetch(`/api/colors/${params.color_id}.json`);
+    let { data } = await response.json();
+    let { id, attributes } = data;
+
+    return { id, ...attributes };
+  }
 }
